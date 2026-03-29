e6c8a2f16a1c4ec25fa86465d601361265ec42c2
vladrybchynskyi <vladyslav.rybchynskyi.ri.2024@lpnu.ua>
2026-03-29 23:23:23 +0300

    code for main

---
+++ lab1/main.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "this" {
+  name           = var.table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = var.hash_key
+
+  attribute {
+    name = var.hash_key
+    type = "S" # "S" означає String (рядок)
+  }
+
+  tags = {
+    Module = "DynamoDB"
+  }
+}
